0d8e1abeadd5ab8e5e0b2cb8a43b4fad4d2e842b
ather1234 <shreypal37@gmail.com>
2025-01-05 13:22:16 -0800

    Update UserRecord.java

--- src/main/java/com/jpmc/midascore/entity/UserRecord.java
+++ src/main/java/com/jpmc/midascore/entity/UserRecord.java
@@ -1,46 +1,66 @@
 package com.jpmc.midascore.entity;
 
 import jakarta.persistence.*;
+import java.math.BigDecimal;
 
 @Entity
-public class UserRecord {
+public class TransactionRecord {
 
     @Id
-    @GeneratedValue()
-    private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "recipient_id", nullable = false)
+    private User recipient;
 
     @Column(nullable = false)
-    private String name;
+    private BigDecimal amount;
 
-    @Column(nullable = false)
-    private float balance;
-
-    protected UserRecord() {
+    protected TransactionRecord() {
     }
 
-    public UserRecord(String name, float balance) {
-        this.name = name;
-        this.balance = balance;
+    public TransactionRecord(User sender, User recipient, BigDecimal amount) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.amount = amount;
     }
 
     @Override
     public String toString() {
-        return String.format("User[id=%d, name='%s', balance='%f'", id, name, balance);
+        return String.format("Transaction[id=%d, sender='%s', recipient='%s', amount='%s']",
+                             id, sender.getName(), recipient.getName(), amount);
     }
 
     public Long getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
+    public User getSender() {
+        return sender;
     }
 
-    public float getBalance() {
-        return balance;
+    public User getRecipient() {
+        return recipient;
     }
 
-    public void setBalance(float balance) {
-        this.balance = balance;
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
     }
 }
