7ac709aa54a5206735f37b5e160151437c561ed5
ather1234 <shreypal37@gmail.com>
2025-01-05 13:19:19 -0800

    Update UserRecord.java

--- src/main/java/com/jpmc/midascore/entity/UserRecord.java
+++ src/main/java/com/jpmc/midascore/entity/UserRecord.java
@@ -1,13 +1,14 @@
 package com.jpmc.midascore.entity;
 
 import jakarta.persistence.*;
+import java.util.List;
 
 @Entity
-public class UserRecord {
+public class User {
 
     @Id
-    @GeneratedValue()
-    private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Column(nullable = false)
     private String name;
@@ -15,17 +16,23 @@ public class UserRecord {
     @Column(nullable = false)
     private float balance;
 
-    protected UserRecord() {
+    @OneToMany(mappedBy = "sender")
+    private List<TransactionRecord> sentTransactions;
+
+    @OneToMany(mappedBy = "recipient")
+    private List<TransactionRecord> receivedTransactions;
+
+    protected User() {
     }
 
-    public UserRecord(String name, float balance) {
+    public User(String name, float balance) {
         this.name = name;
         this.balance = balance;
     }
 
     @Override
     public String toString() {
-        return String.format("User[id=%d, name='%s', balance='%f'", id, name, balance);
+        return String.format("User[id=%d, name='%s', balance='%f']", id, name, balance);
     }
 
     public Long getId() {
@@ -42,5 +49,14 @@ public class UserRecord {
 
     public void setBalance(float balance) {
         this.balance = balance;
+    }
+
+    // Add getters and setters for transactions if needed
+    public List<TransactionRecord> getSentTransactions() {
+        return sentTransactions;
+    }
+
+    public List<TransactionRecord> getReceivedTransactions() {
+        return receivedTransactions;
     }
 }
